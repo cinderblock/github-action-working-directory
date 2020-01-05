0b7c88502ea56d205e54d1a0958581e346432ada
Cameron Tacklind <cameron@tacklind.com>
2020-01-05 03:19:47 -0800

    Add missing file

---
+++ src/commit.ts
@@ -0,0 +1,21 @@
+// Other packages https://github.com/actions/toolkit/blob/master/README.md#packages
+import core from '@actions/core';
+
+import { wait } from './wait';
+
+async function run(): Promise<void> {
+  try {
+    const ms: string = core.getInput('milliseconds');
+    core.debug(`Waiting ${ms} milliseconds ...`);
+
+    core.debug(new Date().toTimeString());
+    await wait(parseInt(ms, 10));
+    core.debug(new Date().toTimeString());
+
+    core.setOutput('time', new Date().toTimeString());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
