d1fee77df6537ad750017429021557e28e284e02
Cameron Tacklind <cameron@tacklind.com>
2020-01-06 03:13:15 -0800

    New dist

--- tests/main.ts
+++ tests/main.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 describe('GitHub Actions Format', () => {
   test('action.yml exists', () => {
-    expect(existsSync(join(__dirname, '..', 'action.yml'))).toBe(true);
+    expect(existsSync(join(__dirname, '..', 'dist', 'action.yml'))).toBe(true);
   });
 
   // TODO: Read action.yml and run main/post on dummy repo/dir
