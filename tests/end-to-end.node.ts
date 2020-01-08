907f724ff60355eb4eb04a26bddf9a6ef5ba7181
Cameron Tacklind <cameron@tacklind.com>
2020-01-08 01:33:35 -0800

    Fix missing and add new e2e test

---
+++ tests/end-to-end.node.ts
@@ -0,0 +1,53 @@
+import { clone } from '../src/utils/clone';
+import rmfr from 'rmfr';
+import { testTempDir } from './utils/testTempDir';
+
+describe('utils/clone', () => {
+  test('clone is a function', () => {
+    expect(typeof clone).toBe('function');
+  });
+
+  const repoUrl = '.';
+  const parentDir = testTempDir(__filename);
+
+  // TODO: figure out why this is hitting the timeout
+  // eslint-disable-next-line @typescript-eslint/promise-function-async
+  beforeAll(() => rmfr(parentDir), 1000);
+  // Cleanup
+  // eslint-disable-next-line @typescript-eslint/promise-function-async
+  afterAll(() => rmfr(parentDir), 1000);
+
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  process.stdout.write = jest.fn();
+
+  test('works with non-existent branch', async () => {
+    const dir = `${parentDir}/clone-non-existent`;
+
+    const result = await clone({
+      repoUrl,
+      branch: 'this-branch-will-never-exist',
+      dir,
+    });
+
+    expect(result).toBe(null);
+
+    expect('this test').toBe('flushed out');
+  });
+
+  test('works with existing branch', async () => {
+    const dir = `${parentDir}/clone-existing`;
+
+    const result = await clone({ repoUrl, branch: 'master', dir });
+
+    expect(result).not.toBe(null);
+    if (result === null) throw new Error('unreachable');
+
+    const head = await result.head();
+
+    expect(head.isValid()).toBe(true);
+
+    expect(!!head.isBranch()).toBe(true);
+
+    expect('this test').toBe('flushed out');
+  });
+});
