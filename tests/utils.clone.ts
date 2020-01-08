dd1cae2ca83f067d2a29f7cbf34f7b6a753aa94a
Cameron Tacklind <cameron@tacklind.com>
2020-01-07 19:14:13 -0800

    Improve cleanup

--- tests/utils.clone.ts
+++ tests/utils.clone.ts
@@ -11,38 +11,32 @@ describe('utils/clone', () => {
   const parentDir = testTempDir(__filename);
 
   // TODO: figure out why this is hitting the timeout
-  afterAll(async () => rmfr(parentDir), 1000);
+  // eslint-disable-next-line @typescript-eslint/promise-function-async
+  beforeAll(() => rmfr(parentDir), 1000);
+  // Cleanup
+  // eslint-disable-next-line @typescript-eslint/promise-function-async
+  afterAll(() => rmfr(parentDir), 1000);
 
   // eslint-disable-next-line @typescript-eslint/unbound-method
   process.stdout.write = jest.fn();
 
   test('clone can be called for non-existent branch', async () => {
     const dir = `${parentDir}/clone-non-existent`;
-    await rmfr(dir); // Make sure we're clean
-    const result = clone({
+
+    const result = await clone({
       repoUrl,
       branch: 'this-branch-will-never-exist',
       dir,
     });
 
-    // cleanup
-    const cleanup = rmfr(dir); // Make sure we're clean
-
-    expect((await result).isValid()).toBe(true);
-
-    await cleanup;
+    expect(result?.isValid()).toBe(true);
   });
 
   test('clone can be called for existing branch', async () => {
     const dir = `${parentDir}/clone-existing`;
-    await rmfr(dir); // Make sure we're clean
-    const result = clone({ repoUrl, branch: 'master', dir });
 
-    // cleanup
-    const cleanup = rmfr(dir); // Make sure we're clean
+    const result = await clone({ repoUrl, branch: 'master', dir });
 
-    expect((await result).isValid()).toBe(true);
-
-    await cleanup;
+    expect(result?.isValid()).toBe(true);
   });
 });
