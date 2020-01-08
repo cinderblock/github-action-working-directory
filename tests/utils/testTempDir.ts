f5cedcacab2e6edb30019bfa2c43546e3ae00417
Cameron Tacklind <cameron@tacklind.com>
2020-01-08 00:06:36 -0800

    Concurrent tests

--- tests/utils/testTempDir.ts
+++ tests/utils/testTempDir.ts
@@ -1,7 +1,11 @@
 import { tmpdir } from 'os';
 import { basename } from 'path';
 
+const runID = Math.random()
+  .toString(36)
+  .substring(2);
+
 export function testTempDir(testName: string, parent?: string): string {
   return `${tmpdir}/github-action-working-directory/${parent ||
-    'dummy-test'}/${basename(testName)}`;
+    `dummy-test-${runID}`}/${basename(testName)}`;
 }
