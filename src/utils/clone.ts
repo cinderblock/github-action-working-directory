aa122f97f11ce141b8d1705e4ccc1860119005b1
Cameron Tacklind <cameron@tacklind.com>
2020-01-08 00:36:07 -0800

    Cleanup debug

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -14,31 +14,27 @@ export async function clone({
   branch,
   debug,
 }: Options): Promise<Reference | null> {
-  if (debug === undefined) {
-    debug = core.debug;
-  }
+  const dbg = debug ?? core.debug;
 
-  debug(`cloning repo(${repoUrl}) into dir(${dir})`);
+  dbg(`cloning repo(${repoUrl}) into dir(${dir})`);
 
   // TODO: Handle empty repoUrl. Find main remote of current repo
 
   const repository = await Clone.clone(repoUrl, dir);
 
-  debug(`Checking out branch(${branch})`);
+  dbg(`Checking out branch(${branch})`);
 
   try {
     const ret = await repository.checkoutBranch(branch);
 
     // TODO: Handle missing branch (initial commit)
 
-    debug(`Branch checked out`);
+    dbg(`Branch checked out`);
 
     return ret;
   } catch (e) {
     if (typeof e !== 'string') throw e;
     if (e !== `no reference found for shorthand '${branch}'`) throw e;
-
-    debug(`Branch does not exist. Creating a new one.`);
 
     return null;
   }
