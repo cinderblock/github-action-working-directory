98b502d3306ba0cd5cfc0525bcc9e42e88113e20
Cameron Tacklind <cameron@tacklind.com>
2020-02-17 00:16:05 -0800

    Improve docs

--- tests/utils/testTempDir.ts
+++ tests/utils/testTempDir.ts
@@ -5,6 +5,13 @@ const runID = Math.random()
   .toString(36)
   .substring(2);
 
+/**
+ * Get a name of a folder to use for testing with.
+ *
+ * @param {string} testName - Name test to create a dedicated folder for
+ * @param {string} parent - Optional test name
+ * @returns {string} The path to the folder with no trailing slash
+ */
 export function testTempDir(testName: string, parent?: string): string {
   return `${tmpdir}/github-action-working-directory/${parent ||
     `dummy-test-${runID}`}/${basename(testName)}`;
